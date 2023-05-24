6243b1328adbb787bb3d6c0dd481f46e543e78e9
hollypacey <48280585+hollypacey@users.noreply.github.com>
2023-05-24 11:09:16 +0100

    Initial commit

---
+++ src/oxrse_unit_conv/units.py
@@ -0,0 +1,32 @@
+from si import *
+from meta.classes import Unit
+
+# second
+minute = Unit(name='minute', abbr='min', si=second, to_si_fun=lambda n: n * 60)
+# min shadows the builtin function 'min'
+
+hour = Unit(name='hour', abbr='h', si=second, to_si_fun=lambda n: n * 3600)
+h = hour
+
+# meter
+kilometer = Unit(name='kilometer', abbr="km", si=meter, to_si_fun=lambda n: n * 1000)
+km = kilometer
+
+mile = Unit(name='mile', abbr='mile', si=meter, to_si_fun=lambda n: n * 1_609.344)
+
+# meter_sq
+
+# meter_cu
+
+# kilogram
+
+pound = Unit(name='pound', abbr='lb', si=kilogram, to_si_fun=lambda n: n * 0.4535924)
+lb = pound
+
+# ampere
+
+# kelvin
+
+# mole
+
+# candela
